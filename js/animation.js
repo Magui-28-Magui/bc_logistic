d9429473f5c16ed4ca02c2c899629ecc255e215b
Magui Campos <88505411+MaguiCampos@users.noreply.github.com>
2023-07-07 17:01:08 -0700

    scroll view

---
+++ js/animation.js
@@ -0,0 +1,22 @@
+const aboutImage = document.getElementById('about_us_image');
+const aboutText = document.getElementById('about_us_text');
+
+const uploadAboutImage = (inputs, observer) => {
+    inputs.forEach(input => {
+        if(input.isIntersecting){
+            console.log('la imagen esta en el viewpoint');
+            input.target.classList.add('visible');
+        }else{
+            input.target.classList.remove('visible');  
+        }
+    });
+};
+//const uploadAboutText = () => {};
+
+const observer = new IntersectionObserver(uploadAboutImage, {
+    root: null,
+    rootMargin:'0px 0px 0px 0px',
+    thresold:1.0,
+ });
+
+ observer.observe(aboutImage);
